3c8c3a7c84fa07bd7ad25f543fdb5625ab0eae70
alexanderwilly <alexanderwillyj@gmail.com>
2024-03-19 14:27:56 +0800

    update 19/3/24 2:27pm

---
+++ script/home.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('.typed', {
+      strings: ["I'm Willy", "A Developer"],
+      loop : true,
+      backSpeed : 80,
+      backDelay : 1500,
+      typeSpeed : 50
+    });
+  });
