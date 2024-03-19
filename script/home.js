a38f7efc816d5ab90ea2bf33e6221c61ce700abd
alexanderwilly <alexanderwillyj@gmail.com>
2024-03-19 20:23:22 +0800

    update 19/3/24 8:23pm

--- script/home.js
+++ script/home.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var typed = new Typed('.typed', {
-      strings: ["I'm Willy", "A Developer"],
+    var typed = new Typed('.typing', {
+      strings: ["Developer", 
+                "Software Engineer",
+                "Cybersecurity"],
       loop : true,
       backSpeed : 80,
       backDelay : 1500,
